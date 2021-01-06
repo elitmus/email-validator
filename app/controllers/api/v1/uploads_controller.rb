9ce1aec3a811dcb7ee7c4aff17091c7c40d8c9ef
prashant-shekhar <toprashantshekhar@gmail.com>
2021-01-06 11:41:06 +0530

    Added Rubocop Gem and secrets.yml.example file

--- app/controllers/api/v1/uploads_controller.rb
+++ app/controllers/api/v1/uploads_controller.rb
@@ -1,17 +1,18 @@
 class Api::V1::UploadsController < ApplicationController
-  def index
-  end
+  def index; end
 
   def create
     original_file = params[:csv_file]
     file_type = original_file.content_type
-    if file_type == "text/csv"
+    if file_type == 'text/csv'
       user_id = params[:userid]
       attachment = Attachment.create(user_id: user_id, csv_file: original_file, processed: false)
       ExportWorker.perform_async(attachment.id)
-      render json: { error: false, message: "File is processing" }
+      render json: { error: false, message: 'File is processing' }
     else
-      render json: { error: true, message: "File format not supported. Please upload CSV file." }, status: :not_acceptable
+      render json: { error: true, message: 'File format not supported. Please upload CSV file.' },
+             status: :not_acceptable
     end
   end
 end
+
